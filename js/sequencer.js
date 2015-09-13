4e62d90cdbb087cfe9cdcce6310891c366b2d454
Roger Spielmann <puncher@braveheart.ch>
2015-09-13 22:44:03 +0200

    adds 16 steps

--- js/sequencer.js
+++ js/sequencer.js
@@ -1,7 +1,24 @@
 app.controller('SeqCtrl', function($scope, $interval, ngAudio) {
 
     //init data
-    $scope.step = {1:{active: false}, 2:{active: false}, 3:{active: false}, 4:{active: false}};
+    $scope.step = {
+        1:{active: false},
+        2:{active: false},
+        3:{active: false},
+        4:{active: false},
+        5:{active: false},
+        6:{active: false},
+        7:{active: false},
+        8:{active: false},
+        9:{active: false},
+        10:{active: false},
+        11:{active: false},
+        12:{active: false},
+        13:{active: false},
+        14:{active: false},
+        15:{active: false},
+        16:{active: false}
+    };
 
     //init sounds
     $scope.sound = ngAudio.load("audio/test.mp3");
@@ -48,7 +65,7 @@ app.controller('SeqCtrl', function($scope, $interval, ngAudio) {
         console.log(lastStep);
 
         curStep ++;
-        if(curStep > 4) {
+        if(curStep > 16) {
             curStep = 1;
         }
     };
